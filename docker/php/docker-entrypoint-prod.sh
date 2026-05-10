e360abf618ebfe80bcc634d1bdb7090922d25676
Kaelian Baudelet <aconique@gmail.com>
2026-05-10 02:42:17 +0200

    chore: setup ci

--- docker/php/docker-entrypoint-prod.sh
+++ docker/php/docker-entrypoint-prod.sh
@@ -3,11 +3,15 @@ set -e
 
 # Run migrations if we are starting the web server
 if [ "$1" = 'apache2-foreground' ]; then
-    echo "[entrypoint] Running Doctrine migrations..."
+    echo "[entrypoint] Clearing cache..."
     if [ "$(id -u)" -eq 0 ] && id www-data >/dev/null 2>&1; then
+        su -s /bin/sh www-data -c "php bin/console cache:clear --no-interaction"
+        echo "[entrypoint] Running Doctrine migrations..."
         su -s /bin/sh www-data -c "php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration"
         su -s /bin/sh www-data -c "php bin/console doctrine:fixtures:load --append --no-interaction"
     else
+        php bin/console cache:clear --no-interaction
+        echo "[entrypoint] Running Doctrine migrations..."
         php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
         php bin/console doctrine:fixtures:load --append --no-interaction
     fi
